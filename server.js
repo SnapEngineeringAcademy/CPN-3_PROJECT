379fcfaaceea5aa2a897658dae4bd19b744ec97a
shadezs <davila_salazar_carlo@student.smc.edu>
2020-08-04 11:38:14 -0700

    inital setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 3030;
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ exrended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+})
+app.get('/', (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(3030, (err) => console.log(`${err ? err: 'Runnig on port 3030'}`), );
